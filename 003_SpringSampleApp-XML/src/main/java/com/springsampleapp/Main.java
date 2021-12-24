f9e7beb59a7adee6a23338b672648a021f5bb6a3
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-24 01:39:59 -0800

    Add new applictionContext.xml Defined two beans. 1. One is SpeakerRepository which don't have any other reference. 2. Another one is SpeakerService which have dependency on SpeakerRepository.
    In this example, we used the SetterInjection.

--- 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/Main.java
+++ 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/Main.java
@@ -2,11 +2,15 @@ package com.springsampleapp;
 
 import com.springsampleapp.service.SpeakerService;
 import com.springsampleapp.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args)
     {
-        var speakerService = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //var speakerService = new SpeakerServiceImpl();
+        SpeakerService speakerService = applicationContext.getBean("speakerService", SpeakerService.class);
         var speakers = speakerService.getAll();
         for (var speaker: speakers)
         {
--- 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/service/SpeakerServiceImpl.java
+++ 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/service/SpeakerServiceImpl.java
@@ -7,10 +7,15 @@ import com.springsampleapp.repository.SpeakerRepositoryImpl;
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository speakerRepository = new SpeakerRepositoryImpl();
+    private SpeakerRepository speakerRepository;
     @Override
     public List<Speaker> getAll()
     {
         return this.speakerRepository.getAllSpeaker();
     }
+    // for the setter injection, we should have this setterMethod. Otherwise, it will throw error.
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
+
 }
