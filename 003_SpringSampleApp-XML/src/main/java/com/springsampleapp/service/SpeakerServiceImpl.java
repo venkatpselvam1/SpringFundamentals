25167e0c4babdae795b92236577be7600d41e83d
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-24 01:48:16 -0800

    Constructor injection with XML configuration in Spring

--- 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/service/SpeakerServiceImpl.java
+++ 003_SpringSampleApp-XML/src/main/java/com/springsampleapp/service/SpeakerServiceImpl.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository speakerRepository;
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository)
+    {
+        this.speakerRepository = speakerRepository;
+    }
     @Override
     public List<Speaker> getAll()
     {
         return this.speakerRepository.getAllSpeaker();
     }
+/*
     // for the setter injection, we should have this setterMethod. Otherwise, it will throw error.
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
         this.speakerRepository = speakerRepository;
     }
-
+*/
 }
